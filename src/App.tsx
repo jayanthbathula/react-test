2076df9e4a79b9d177c93b1002261d72c6833842
Jayanth Bathula <bathula.jayanth@gmail.com>
2023-04-18 20:43:28 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,28 @@
-import React from 'react';
+import React, { useState, useEffect} from 'react';
 import './App.css';
+import Card from './components/Card';
 
 function App() {
+  const [cardList, setCardList] = useState([]);
+
+  useEffect(() => {
+    // get cards list
+    const data = fetch('https://jsonplaceholder.typicode.com/users');
+    data.then(res => res.json())
+    .then(result => {
+      setCardList(result)
+    })
+  }, [])
+
+  function getCardsList() {
+    return cardList.map((card: any) => {
+      return <Card key={card.id} data={card} />
+    })
+  }
+
   return (  
     <div className="App">
+      {cardList && cardList.length ? getCardsList() : null}
     </div>
   );
 }
